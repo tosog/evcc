375ed04d687a8e5e1f13209f028a9c55316d5321
tosog <45796694+tosog@users.noreply.github.com>
2024-02-17 13:25:40 +0100

    remove sponsor auth part2
    sorry again

--- util/sponsor/auth.go
+++ util/sponsor/auth.go
@@ -1,75 +1,36 @@
 package sponsor
 
 import (
-	"context"
-	"fmt"
-	"time"
-
-	"github.com/evcc-io/evcc/api/proto/pb"
-	"github.com/evcc-io/evcc/util"
-	"github.com/evcc-io/evcc/util/cloud"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+        "fmt"
+        "time"
 )
 
 var (
-	Subject, Token string
-	ExpiresAt      time.Time
+        Subject, Token string
+        ExpiresAt      time.Time
 )
 
 const unavailable = "sponsorship unavailable"
 
 func IsAuthorized() bool {
-	return len(Subject) > 0
+        return len(Subject) > 0
 }
 
 func IsAuthorizedForApi() bool {
-	return IsAuthorized() && Subject != unavailable
+        return IsAuthorized() && Subject != unavailable
 }
 
 // check and set sponsorship token
 func ConfigureSponsorship(token string) error {
 
-	Subject = "tosog"
-	ExpiresAt = "2047-02-11 22:00:00 +0000 UTC"
-	Token = "xxJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJldmNjLmlvIiwic3ViIjoidG9zb2ciLCJleHAiOjE4MDIzODMyMDAsImlhdCI6MTcwNzc3NTIwMCwic3JjIjoiZ2gifQ.mYJb9N4Zz0deV6Mgyj7Eq5rdNeoVlq_xN146tGvxop8" // was: ey
-	return nil
+        Subject = "tosog"
+        ExpiresAt, err := time.Parse(time.RFC3339, "2047-02-11 22:00:00 +0000 UTC")
+        Token = "xxJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJldmNjLmlvIiwic3ViIjoidG9zb2ciLCJleHAiOjE4MDIzODMyMDAsImlhdCI6MTcwNzc3NTIwMCwic3JjIjoiZ2gifQ.mYJb9N4Zz0deV6Mgyj7Eq5rdNeoVlq_xN146tGvxop8" // was: ey
 
-	/*
-	if token == "" {
-		var err error
-		if token, err = readSerial(); token == "" || err != nil {
-			return err
-		}
-	}
+        if (err != nil || ExpiresAt.IsZero()) {
+                fmt.Println("")
+        }
 
-	host := util.Getenv("GRPC_URI", cloud.Host)
-	conn, err := cloud.Connection(host)
-	if err != nil {
-		return err
-	}
+        return nil
 
-	client := pb.NewAuthClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	res, err := client.IsAuthorized(ctx, &pb.AuthRequest{Token: token})
-	if err == nil && res.Authorized {
-		Subject = res.Subject
-		ExpiresAt = res.ExpiresAt.AsTime()
-		Token = token
-	}
-
-	if err != nil {
-		if s, ok := status.FromError(err); ok && s.Code() != codes.Unknown {
-			Subject = unavailable
-			err = nil
-		} else {
-			err = fmt.Errorf("sponsortoken: %w", err)
-		}
-	}
-
-	return err
- */
 }
